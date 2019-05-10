a19b1761d72056cf4c394f1f60f3f44654110ebe
veerapath <veerapath784@gmail.com>
2019-05-10 13:57:22 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/homepage', function () {
     return view('homepage');
 });
 
